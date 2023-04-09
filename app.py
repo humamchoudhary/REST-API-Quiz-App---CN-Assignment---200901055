e93f62bcbc3f0e64b3a201b453f02a3861e541a0
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-04-09 15:03:27 +0500

    fixed

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect
 import json
 import random
-
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__)
+quiz_data = []
 
 # Load questions from JSON file
 with open('questions.json') as f:
     data = json.load(f)
+    data = data["questions"]
+
+
 
 # Endpoint to get random questions for the quiz
-@app.route('/quiz', methods=['POST'])
+@app.route('/quiz', methods=['POST', "GET"])
 def quiz():
-    num_questions = int(request.form['num_questions'])
-    questions = random.sample(data, num_questions)
-    quiz_data = []
-    for q in questions:
-        q_data = {}
-        q_data['question'] = q['question']
-        q_data['options'] = q['options']
-        q_data['answer'] = q['answer']
-        quiz_data.append(q_data)
-    return jsonify({'quiz_data': quiz_data})
+    global quiz_data
+    if (request.method == 'POST'):
+        quiz_data = []
+
+        num_questions = int(request.form['num_questions'])
+
+        questions = random.sample(
+            data, min(num_questions, len(data)))
+        for q in questions:
+            q_data = {}
+            q_data['question'] = q['question']
+            q_data['options'] = q['options']
+            q_data['answer'] = q['answer']
+            quiz_data.append(q_data)
+        return jsonify({'quiz_data': quiz_data})
+    else:
+        return render_template('quiz.html')
+
 
 # Endpoint to check answers and calculate score
+
+@app.route('/')
+def index():
+    return redirect('/quiz')
+
+
 @app.route('/quiz', methods=['PUT'])
 def submit():
-    quiz_data = request.get_json()
+    quiz_ans_data = request.get_json()
+    print(quiz_ans_data)
     score = 0
     results = []
-    for i, q in enumerate(data):
+    for i, q in enumerate(quiz_data):
         q_result = {}
         q_result['question'] = q['question']
-        q_result['selected_answer'] = quiz_data[str(i)]
+
+        q_result['selected_answer'] = quiz_ans_data[str(i)]
         q_result['correct_answer'] = q['answer']
         if q_result['selected_answer'] == q['answer']:
             q_result['correct'] = True
@@ -39,9 +58,10 @@ def submit():
         else:
             q_result['correct'] = False
         results.append(q_result)
-    score_percentage = round(score / len(data) * 100, 2)
+    print(score)
+    score_percentage = round(score / len(quiz_ans_data) * 100, 2)
     return jsonify({'score': score_percentage, 'quiz_data': results})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=3000)
